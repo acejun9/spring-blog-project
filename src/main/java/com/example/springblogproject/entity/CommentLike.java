5d2f008e59d6534a0d6def4b71e0b93f7ac2d086
seongjun <seongjuni1@naver.com>
2023-01-02 21:32:39 +0900

    fix : entity-CommentLike 전체 수정

--- src/main/java/com/example/springblogproject/entity/CommentLike.java
+++ src/main/java/com/example/springblogproject/entity/CommentLike.java
@@ -4,32 +4,20 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Entity
 @Getter
 @NoArgsConstructor
 public class CommentLike {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "comment_id", nullable = false)
+    private Comment comment;
+    private String username;
 
-    @Column
-    private String commentId;
-
-    //User로 받아올지?? String으로 해서 username만 받아올지??
-    //이게 맞나 모지??
-    @Column
-    private List<String> likeUsers = new ArrayList<>();
-
-    @Column
-    private Long likeCount;
-
-    //일단 코멘트 만들어보고 추가하기
-    public CommentLike(String commentId, String username) {
-        this.commentId = commentId;
-        this.likeUsers = new ArrayList<>();
-        likeUsers.add(username); // ????
+    public CommentLike(Comment comment, String username){
+        this.comment = comment;
+        this.username = username;
     }
 }
