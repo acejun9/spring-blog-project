7a005574fd5e090f193842d33bb4410ca7391ea2
SangHwan <skyi0909@naver.com>
2023-01-02 19:59:32 +0900

    feat - dto - SignupRequestDto 생성

---
+++ src/main/java/com/example/springblogproject/dto/SignupRequestDto.java
@@ -0,0 +1,33 @@
+package com.example.springblogproject.dto;
+
+import jakarta.annotation.Nonnull;
+import jakarta.annotation.Nullable;
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@NoArgsConstructor
+public class SignupRequestDto {
+
+
+    //유효성검사
+    @NotNull(message="이름을 입력해주세요")
+    private String username;
+    @NotNull(message="비밀번호를 입력해주세요")
+    private String password;
+    private boolean admin = false;
+    private String adminToken = "";
+
+    //Setter대신해서
+    @Builder
+    public SignupRequestDto(String username, String password)
+    {
+        this.username = username;
+        this.password = password;
+    }
+
+}
