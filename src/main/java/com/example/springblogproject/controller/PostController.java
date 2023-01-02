0daa8ff0d597857c2df789025aec91cfa7dfe498
Yoon Yeojune <youj11@naver.com>
2023-01-02 14:38:18 +0900

    feat : dto - PostResponseDto 생성

--- src/main/java/com/example/springblogproject/controller/PostController.java
+++ src/main/java/com/example/springblogproject/controller/PostController.java
@@ -1,19 +1,24 @@
 package com.example.springblogproject.controller;
 
 import com.example.springblogproject.dto.PostRequestDto;
+import com.example.springblogproject.dto.PostResponseDto;
 import com.example.springblogproject.entity.Post;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 public class PostController {
 
-    @PostMapping("/api/posts")
-    public Post createPost(@RequestBody PostRequestDto List<PostResponseDto>) {
-
+    @GetMapping("/api/posts")
+    public ModelAndView posts() {
+        return new ModelAndView(List<PostResponseDto>);
     }
 
 }
