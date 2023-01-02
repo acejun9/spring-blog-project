a8e7aa5dd78570156e55586d7be8e4b57a1e6d7a
JYMoon <damary016@gmail.com>
2023-01-02 15:57:31 +0900

    feat: dto - CommentRequestDto, CommentResponseDto 추가

---
+++ src/main/java/com/example/springblogproject/dto/CommentRequestDto.java
@@ -0,0 +1,8 @@
+package com.example.springblogproject.dto;
+
+import lombok.Getter;
+
+@Getter
+public class CommentRequestDto {
+    private String content;
+}
---
+++ src/main/java/com/example/springblogproject/dto/CommentResponseDto.java
@@ -0,0 +1,27 @@
+package com.example.springblogproject.dto;
+
+import com.example.springblogproject.entity.Comment;
+import com.example.springblogproject.entity.CommentLike;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class CommentResponseDto {
+    private Long id;
+    private String content;
+    private String username;
+    private LocalDateTime createdAt;
+    private LocalDateTime modifiedAt;
+    private Long commentLike;
+
+    public CommentResponseDto(Comment comment) {
+        this.id = comment.getId();
+        this.content = comment.getContent();
+        this.username = comment.getUsername();
+        this.createdAt = comment.getCreatedAt();
+        this.modifiedAt = comment.getModifiedAt();
+        this.commentLike = comment.getCommentLike().getLikeCount();
+    }
+
+}
