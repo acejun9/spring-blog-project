52978ec2a0362004dd06548a8dd54b8e14713869
Yoon Yeojune <youj11@naver.com>
2023-01-02 17:25:31 +0900

    feat : repository - PostRepository 생성 refactor : dto - PostRequestDto, PostResponseDto 수정, entity - Post 수정

---
+++ src/main/java/com/example/springblogproject/repository/PostRepository.java
@@ -0,0 +1,10 @@
+package com.example.springblogproject.repository;
+
+import com.example.springblogproject.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+    List<Post> findAllByOrderByCreatedAtDesc();
+}
