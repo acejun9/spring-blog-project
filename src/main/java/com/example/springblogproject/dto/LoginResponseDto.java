a50a9f1344731f221264ae3836d81aa91911df94
SangHwan <skyi0909@naver.com>
2023-01-03 12:54:55 +0900

    feat - LoginResponseDto 생성

---
+++ src/main/java/com/example/springblogproject/dto/LoginResponseDto.java
@@ -0,0 +1,19 @@
+package com.example.springblogproject.dto;
+
+import com.example.springblogproject.entity.User;
+import com.example.springblogproject.util.UserRoleEnum;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class LoginResponseDto {
+    private String username;
+    private UserRoleEnum role;
+    @Builder
+    public LoginResponseDto(String username,UserRoleEnum role) {
+        this.username = username;
+        this.role = role;
+    }
+}
