ee1f4ff8f11df717ccdbeb81abff5da8f3eb5313
seongjun <seongjuni1@naver.com>
2023-01-06 00:51:03 +0900

    refactor : 좋아요, 좋아요 취소 기능 분리

--- src/main/java/com/example/springblogproject/controller/CommentController.java
+++ src/main/java/com/example/springblogproject/controller/CommentController.java
@@ -2,6 +2,9 @@ package com.example.springblogproject.controller;
 
 import com.example.springblogproject.dto.CommentRequestDto;
 import com.example.springblogproject.dto.CommentResponseDto;
+import com.example.springblogproject.entity.CommentLike;
+import com.example.springblogproject.entity.PostLike;
+import com.example.springblogproject.repository.CommentLikeRepository;
 import com.example.springblogproject.security.UserDetailsImpl;
 import com.example.springblogproject.service.CommentService;
 import com.example.springblogproject.util.UserRoleEnum;
@@ -12,11 +15,14 @@ import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/comments")
 public class CommentController {
     private final CommentService commentService;
+    private final CommentLikeRepository commentLikeRepository;
 
     @PostMapping("/{postId}")
     public ResponseEntity<CommentResponseDto> createComment(@PathVariable Long postId, @RequestBody CommentRequestDto commentRequestDto, @AuthenticationPrincipal UserDetailsImpl userDetails) {
@@ -47,8 +53,21 @@ public class CommentController {
         return new ResponseEntity<>("해당 댓글이 삭제되었습니다.", HttpStatus.OK);
     }
 
-    @PatchMapping("/{id}")
-    public ResponseEntity<String> updateLikeComment(@PathVariable Long id, @AuthenticationPrincipal UserDetailsImpl userDetails) {
-        return new ResponseEntity<>(commentService.updateLikeComment(id, userDetails.getUsername()), HttpStatus.OK);
+    @PostMapping("/{id}/like")
+    public ResponseEntity<String> likeComment(@PathVariable Long id, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        Optional<CommentLike> commentLike = commentLikeRepository.findByUsernameAndCommentId(userDetails.getUsername(),id);
+        if(commentLike.isPresent()){
+            throw new IllegalArgumentException("already did like");
+        }
+        return new ResponseEntity<>(commentService.likeComment(id, userDetails.getUsername()), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}/like")
+    public ResponseEntity<String> cancelLikedComment(@PathVariable Long id, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        Optional<CommentLike> commentLike = commentLikeRepository.findByUsernameAndCommentId(userDetails.getUsername(),id);
+        if(commentLike.isEmpty()){
+            throw new IllegalArgumentException("you didn't like");
+        }
+        return new ResponseEntity<>(commentService.cancelLikedComment(id, userDetails.getUsername()), HttpStatus.OK);
     }
 }
